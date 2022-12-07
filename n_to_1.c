429edd750a3013101fe6eee6f815f109465d8993
mvsarvesha <63770211+mvsarvesha@users.noreply.github.com>
2022-12-08 00:30:45 +0530

    #include<stdio.h> #include<math.h> int main(){     int n,i;
        scanf("%d",&n);
        
        for(i=n;i>0;i--) {
            printf("%d ",i);
            
        }
    }

---
+++ n_to_1.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<math.h>
+int main(){
+    int n,i;
+    
+    scanf("%d",&n);
+    
+    for(i=n;i>0;i--) {
+        printf("%d ",i);
+        
+    }
+}    
+        
+    
