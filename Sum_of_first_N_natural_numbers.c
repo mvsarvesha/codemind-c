a882ae3d6c9af5be8f224c4e7b31ef474b42a778
mvsarvesha <63770211+mvsarvesha@users.noreply.github.com>
2022-12-08 00:28:12 +0530

    #include<stdio.h> #include<math.h> int main(){     int n,i;     int sum;     scanf("%d",&n);     sum=0;     for(i=1;i<=n;i++) {         sum +=i;
        }
        printf("%d",sum);
            
        
    }

---
+++ Sum_of_first_N_natural_numbers.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<math.h>
+int main(){
+    int n,i;
+    int sum;
+    scanf("%d",&n);
+    sum=0;
+    for(i=1;i<=n;i++) {
+        sum +=i;
+        
+    }
+    printf("%d",sum);
+        
+    
+}
