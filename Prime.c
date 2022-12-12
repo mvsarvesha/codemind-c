95345e14d0f558706a23adb5434ee9ddeb8870e0
mvsarvesha <63770211+mvsarvesha@users.noreply.github.com>
2022-12-12 19:00:11 +0530

    #include <stdio.h>
    int main() {
    
      int n, i, flag = 0;
      
      scanf("%d", &n);
    
      // 0 and 1 are not prime numbers
      // change flag to 1 for non-prime number
      if (n == 0 || n == 1)
        flag = 1;
    
      for (i = 2; i <= n / 2; ++i) {
    
        // if n is divisible by i, then n is not prime
        // change flag to 1 for non-prime number
        if (n % i == 0) {
          flag = 1;
          break;
        }
      }
    
      // flag is 0 for prime numbers
      if (flag == 0)
        printf("Prime");
      else
        printf("Not Prime");
    
      
    }

--- Prime.c
+++ Prime.c
@@ -1,19 +1,31 @@
-#include<stdio.h>  
-int main(){    
-int n,i,m=0,flag=0;    
-  
-scanf("%d",&n);    
-m=n/2;    
-for(i=2;i<=m;i++)    
-{    
-if(n%i==0)    
-{    
-printf("Not Prime");    
-flag=1;    
-break;    
-}    
-}    
-if(flag==0)    
-printf("Prime");     
+#include <stdio.h>
 
- }   
+int main() {
+
+  int n, i, flag = 0;
+  
+  scanf("%d", &n);
+
+  // 0 and 1 are not prime numbers
+  // change flag to 1 for non-prime number
+  if (n == 0 || n == 1)
+    flag = 1;
+
+  for (i = 2; i <= n / 2; ++i) {
+
+    // if n is divisible by i, then n is not prime
+    // change flag to 1 for non-prime number
+    if (n % i == 0) {
+      flag = 1;
+      break;
+    }
+  }
+
+  // flag is 0 for prime numbers
+  if (flag == 0)
+    printf("Prime");
+  else
+    printf("Not Prime");
+
+  
+}
