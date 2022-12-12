4f175b52a84b41c1c64211fe71d4f77dad733685
mvsarvesha <63770211+mvsarvesha@users.noreply.github.com>
2022-12-12 15:52:40 +0530

    #include<stdio.h> int main(){     int n,i,j=0;     scanf("%d",&n);     for (i=1;i< n;i++){         if(n%i==0) {             j +=i;
            }
        }
        printf("%d",j);
        
    }

---
+++ _sum_of_factors.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(){
+    int n,i,j=0;
+    scanf("%d",&n);
+    for (i=1;i< n;i++){
+        if(n%i==0) {
+            j +=i;
+            
+        }
+    }
+    printf("%d",j);
+    
+}
