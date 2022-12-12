4ccf8d73a2acb53f59bd396dd7daf745faf24ea7
mvsarvesha <63770211+mvsarvesha@users.noreply.github.com>
2022-12-12 15:36:09 +0530

    #include <stdio.h>
    int main() {
        float radius, perimeter, area;
    
        scanf("%f", &radius);
    
      area = 3.14 * radius * radius;
     // printf("%0.2f", area);
      perimeter = 2 * 3.14 * radius;
      printf("%0.2f
    %0.2f",area, perimeter);
    }

--- Area_and_Perimeter_of_Circle.c
+++ Area_and_Perimeter_of_Circle.c
@@ -1,16 +1,15 @@
 #include <stdio.h>
 
-#define PI 3.14f /* Define the value of pie */
+
 
 int main() {
     float radius, perimeter, area;
 
-    scanf("%f", & radius);
+    scanf("%f", &radius);
 
   area = 3.14 * radius * radius;
-  printf("%0.2f
-", area);
+ // printf("%0.2f", area);
   perimeter = 2 * 3.14 * radius;
   printf("%0.2f
-", perimeter);
+%0.2f",area, perimeter);
 }
