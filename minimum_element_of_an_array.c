3251f763ef1879882ea5f1af6a4eda1145bf74e3
mvsarvesha <63770211+mvsarvesha@users.noreply.github.com>
2022-12-13 06:19:57 +0530

    #include <stdio.h> int main() {   int n;   double arr[100];   //printf("Enter the number of elements (1 to 100): ");   scanf("%d", &n);
      for (int i = 0; i < n; ++i) {
        
        scanf("%lf", &arr[i]);
      }
    
      // storing the largest number to arr[0]
      for (int i = 1; i < n; ++i) {
        if (arr[0] > arr[i]) {
          arr[0] = arr[i];
        }
      }
    
      printf("%.0lf", arr[0]);
    
     
    }

---
+++ minimum_element_of_an_array.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+int main() {
+  int n;
+  double arr[100];
+  //printf("Enter the number of elements (1 to 100): ");
+  scanf("%d", &n);
+
+  for (int i = 0; i < n; ++i) {
+    
+    scanf("%lf", &arr[i]);
+  }
+
+  // storing the largest number to arr[0]
+  for (int i = 1; i < n; ++i) {
+    if (arr[0] > arr[i]) {
+      arr[0] = arr[i];
+    }
+  }
+
+  printf("%.0lf", arr[0]);
+
+ 
+}
