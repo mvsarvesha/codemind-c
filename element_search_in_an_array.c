23d42d0e19f743942c5a4e42e1d872fac8d91dc2
mvsarvesha <63770211+mvsarvesha@users.noreply.github.com>
2022-12-13 21:24:19 +0530

    #include <stdio.h>
    #define MAX_SIZE 100  // Maximum array size
    
    int main()
    {
        int arr[MAX_SIZE];
        int size, i, toSearch, found;
    
        /* Input size of array */
        //printf("Enter size of array: ");
        scanf("%d", &size);
    
        /* Input elements of array */
        //printf("Enter elements in array: ");
        for(i=0; i<size; i++)
        {
            scanf("%d", &arr[i]);
        }
    
        //printf("
    Enter element to search: ");
        scanf("%d", &toSearch);
    
        /* Assume that element does not exists in array */
        found = 0; 
        
        for(i=0; i<size; i++)
        {
            /* 
             * If element is found in array then raise found flag
             * and terminate from loop.
             */
            if(arr[i] == toSearch)
            {
                found = 1;
                break;
            }
        }
    
        /*
         * If element is not found in array
         */
        if(found == 1)
        {
            printf("True");
        }
        else
        {
            printf("False");
        }
    
       
    }

---
+++ element_search_in_an_array.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+
+#define MAX_SIZE 100  // Maximum array size
+
+int main()
+{
+    int arr[MAX_SIZE];
+    int size, i, toSearch, found;
+
+    /* Input size of array */
+    //printf("Enter size of array: ");
+    scanf("%d", &size);
+
+    /* Input elements of array */
+    //printf("Enter elements in array: ");
+    for(i=0; i<size; i++)
+    {
+        scanf("%d", &arr[i]);
+    }
+
+    //printf("
+Enter element to search: ");
+    scanf("%d", &toSearch);
+
+    /* Assume that element does not exists in array */
+    found = 0; 
+    
+    for(i=0; i<size; i++)
+    {
+        /* 
+         * If element is found in array then raise found flag
+         * and terminate from loop.
+         */
+        if(arr[i] == toSearch)
+        {
+            found = 1;
+            break;
+        }
+    }
+
+    /*
+     * If element is not found in array
+     */
+    if(found == 1)
+    {
+        printf("True");
+    }
+    else
+    {
+        printf("False");
+    }
+
+   
+}
