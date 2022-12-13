02cd2d195561920c43a0570f607a6198e451595d
mvsarvesha <63770211+mvsarvesha@users.noreply.github.com>
2022-12-13 21:40:09 +0530

    #include <stdio.h> #include<math.h> #define MAX_SIZE 100  // Maximum array size
    int main()
    {
        int arr[MAX_SIZE];
        int size, i,odd=0,even=0;
    
        /* Input size of array */
       
        scanf("%d", &size);
    
        /* Input elements of array */
        
        for(i=0; i<size; i++)
        {
            scanf("%d", &arr[i]);
            if(i%2 ==0) even+=arr[i];
            else odd +=arr[i];
        }
        printf("%d",abs(odd-even));
    
    }

---
+++ Absolute_difference_b/w_sum_of_even_and_sum_of_odd_indexed_elements.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include<math.h>
+#define MAX_SIZE 100  // Maximum array size
+
+int main()
+{
+    int arr[MAX_SIZE];
+    int size, i,odd=0,even=0;
+
+    /* Input size of array */
+   
+    scanf("%d", &size);
+
+    /* Input elements of array */
+    
+    for(i=0; i<size; i++)
+    {
+        scanf("%d", &arr[i]);
+        if(i%2 ==0) even+=arr[i];
+        else odd +=arr[i];
+    }
+    printf("%d",abs(odd-even));
+
+}
